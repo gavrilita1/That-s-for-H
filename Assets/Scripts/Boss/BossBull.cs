0b0a62678629a1195994fed0865cace6f735b6cf
Gavrilita Rolland <gavrilita_rolland@yahoo.com>
2020-04-11 23:55:10 +0300

    Update BullBoss
    Update BullBoss animations and scripts:
    BossBull, BossBullM, BossBull_Health, BossBull_Run, BossBull_Weapon

---
+++ Assets/Scripts/Boss/BossBull.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBull : MonoBehaviour
+{
+
+    public int health;
+    public float speed;
+
+    private Animator anim;
+    public GameObject bloodEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isRunning", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+    }
+
+    public void TakeDamageEnemy(int damage)
+    {
+        //Instantiate(bloodEffect, transform.position, Quaternion.identity);
+        health = health - damage;
+        Debug.Log("damage Taken " + health);
+    }
+}
---
+++ Assets/Scripts/Boss/BossBull_Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBull : MonoBehaviour
+{
+    public Animator animator;
+
+    public int health = 500;
+    public HealthBar health_Bar;
+
+    public GameObject deathEffect;
+
+    public bool isInvulnerable = false;
+
+    public void TakeDamageEnemy(int damage)
+    {
+        if (isInvulnerable)
+            return;
+        animator.SetTrigger("Hurt");
+
+        health -= damage;
+        Debug.Log("BOSS  " + health);
+        //if (health <= 200)
+        //{
+        //    GetComponent<Animator>().SetBool("IsEnraged", true);
+        //}
+
+        health_Bar.SetHealth(health);
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("PLAYER Dead");
+        animator.SetBool("IsDead", true);
+        //Instantiate(deathEffect, transform.position, Quaternion.identity);
+        GetComponent<Rigidbody2D>().mass = 0;
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
+        this.enabled = false;
+        //Destroy(gameObject);
+    }
+
+}
